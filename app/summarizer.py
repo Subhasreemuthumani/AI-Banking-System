6791160f2a90f62aaad954b41c58fb3fc428805a
Subhasreemuthumani <msubhasree251@gmail.com>
2026-04-15 19:10:10 +0530

    Update summarizer.py

--- app/summarizer.py
+++ app/summarizer.py
@@ -1,6 +1,6 @@
 from transformers import pipeline
 
-summarizer = pipeline("summarization")
+summarizer = pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")
 
 def summarize_complaint(text):
 
